d4eae28ec24bf6bd530afbd3ae7ee0246991b45b
Marcel Kummer <marcel@froglogic.com>
2017-12-14 10:16:12 +0100

    Complete makeover for day 10 part 2 - but it works.

--- 12-10/12-10/main.swift
+++ 12-10/12-10/main.swift
@@ -1,51 +1,57 @@
-func parse(input: String) -> [Int] {
-	let steps = input.split(separator: ",")
-	var lengths = Array(repeating: 0, count: steps.count)
-	for i in 0..<steps.count {
-		guard let val = Int(steps[i]) else {
-			print("Invalid input: \(steps[i])")
-			return []
-		}
+import Foundation // for String(format:)
 
-		lengths[i] = val
+func parse(input: String) -> [UInt8] {
+    var lengths: [UInt8] = []
+    
+	for c in input {
+        guard let scalar = c.unicodeScalars.first else {
+            print("No scalars in \(c)")
+            continue
+        }
+        if !scalar.isASCII {
+            print("Non-ASCII scalar: \(scalar)")
+        }
+        
+		lengths.append(UInt8(scalar.value))
 	}
 
-	return lengths
+	return lengths + [17, 31, 73, 47, 23]
 }
 
-func initList(length: Int) -> [Int] {
-	var list = Array<Int>(repeating: 0, count: length)
-	var val = 0
-
+func initList(length: Int) -> [UInt8] {
+	var list = Array<UInt8>(repeating: 0, count: length)
+    
 	for i in 0..<list.count {
-		list[i] = val
-		val += 1
+		list[i] = UInt8(i)
 	}
 
 	return list
 }
 
-func tieKnots(list original: [Int], lengths: [Int]) -> [Int] {
-	var list = original
-	var cur = 0
-	var skip = 0
+func tieKnots(lengths: [UInt8]) -> [UInt8] {
+	var list = initList(length: 256)
+    var cur: Int = 0
+    var skip: Int = 0
 
-	for length in lengths {
-		reverse(list: &list, range: cur..<(cur + length))
-		cur += (length + skip) % list.count
-		skip += 1 % list.count
-	}
+    for _ in 0..<64 {
+        for length in lengths {
+            let intLength = Int(length)
+            reverse(list: &list, range: cur..<(cur + intLength))
+            cur += (Int(length) + skip) % list.count
+            skip += 1 % list.count
+        }
+    }
 
 	return list
 }
 
-func reverse(list: inout [Int], range: Range<Int>) {
+func reverse(list: inout [UInt8], range: Range<Int>) {
 	var front = range.lowerBound
 	var back = range.upperBound - 1
-	var buf = 0
-
+    var buf: UInt8 = 0
+    
 	while front < back {
-		buf = list[front % list.count]
+        buf = list[front % list.count]
 		list[front % list.count] = list[back % list.count]
 		list[back % list.count] = buf
 		front += 1
@@ -53,14 +59,38 @@ func reverse(list: inout [Int], range: Range<Int>) {
 	}
 }
 
+func densify(hash sparse: [UInt8]) -> [UInt8] {
+    var dense = Array<UInt8>(repeating: 0, count: 16)
+    
+    for i in 0..<sparse.count {
+        dense[i / 16] ^= sparse[i]
+    }
+    
+    return dense
+}
+
+func toHex(hash: [UInt8]) -> String {
+    var out = ""
+    
+    for i in hash {
+        out += String(format: "%02x", i)
+    }
+    
+    return out
+}
+
 var inputs = [
-	5: "3,4,1,5",
-	256: "88,88,211,106,141,1,78,254,2,111,77,255,90,0,54,205"
+    "",
+    "AoC 2017",
+    "1,2,3",
+    "1,2,4",
+	"88,88,211,106,141,1,78,254,2,111,77,255,90,0,54,205"
 ]
 
-for (length, lengths) in inputs {
-	let lengths = parse(input: lengths)
-	let list = initList(length: length)
-	let tied = tieKnots(list: list, lengths: lengths)
-	print("Tied: \(tied) Magic: \(tied[0] * tied[1])")
+for input in inputs {
+	let lengths = parse(input: input)
+	let sparse = tieKnots(lengths: lengths)
+    let dense = densify(hash: sparse)
+    let hex = toHex(hash: dense)
+	print("Tied: \(hex)")
 }
