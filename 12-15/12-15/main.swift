11b7f8c3a684be39f450a2d39dabbeecb9e4d2b7
Marcel Kummer <marcel@nightsoul.org>
2017-12-15 13:58:48 +0100

    Add solution for day 15 part 2

--- 12-15/12-15/main.swift
+++ 12-15/12-15/main.swift
@@ -3,24 +3,29 @@ import Foundation
 class Generator {
 	var last: Int
 	let factor: Int
+	let multiple: Int
 
-	init(seed: Int, factor: Int) {
+	init(seed: Int, factor: Int, multiple: Int) {
 		self.last = seed
 		self.factor = factor
+		self.multiple = multiple
 	}
 
 	func next() -> Int {
-		last = (last * factor) % 2147483647
+		repeat {
+			last = (last * factor) % 2147483647
+		} while last % multiple != 0;
 		return last
 	}
 }
 
 func solve1(_ input: [Int]) -> Int {
-	let a = Generator(seed: input[0], factor: 16807)
-	let b = Generator(seed: input[1], factor: 48271)
+	let a = Generator(seed: input[0], factor: 16807, multiple: 4)
+	let b = Generator(seed: input[1], factor: 48271, multiple: 8)
 	var matches = 0
 
-	for _ in 0..<40000000 {
+	for _ in 0..<5000000 {
+		//print("a: \(a.next()) b: \(b.next())")
 		if a.next() & 0x000000000000ffff == b.next() & 0x000000000000ffff {
 			matches += 1
 		}
